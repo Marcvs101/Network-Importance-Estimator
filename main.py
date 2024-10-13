76716731989d2be14dde823db9cac4409a542d87
Marcvs101 <marcocuoci@gmail.com>
2024-10-13 20:43:11 +0200

    Graph generation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import json
 from scapy.all import *
 
+import networkx as nx
+import plotly.graph_objects as go
+
 
 
 cap = rdpcap('input/input.pcapng')
@@ -91,3 +94,63 @@ f.close()
 f=open(file="output/ip_count_weighted.json",mode="w",encoding="utf-8")
 f.write(json.dumps({"src_to_count":ip_src_count_weighted,"dst_to_count":ip_dst_count_weighted,"src_to_dst_to_count":ip_src_dst_route_count_weighted}))
 f.close()
+
+
+
+# NETWORKX
+network_graph = nx.DiGraph()
+max_weight = 0
+
+for src in ip_src_dst_route_count_weighted:
+    network_graph.add_node(src)
+    for dst in ip_src_dst_route_count_weighted[src]:
+        network_graph.add_node(dst)
+        network_graph.add_edge(src,dst,weight=ip_src_dst_route_count_weighted[src][dst])
+        max_weight = max(max_weight,ip_src_dst_route_count_weighted[src][dst])
+
+
+centrality_metric = nx.degree_centrality(network_graph)
+
+
+# Draw graphs
+def color_centrality(node):
+    return "rgb("+str((1-centrality_metric[node])*255)+","+str((centrality_metric[node])*255)+",0)"
+
+
+
+def print_graph(title,node_color_function,debug_data):
+    pos = nx.spring_layout(network_graph)
+
+    fig = go.Figure()
+    for u, v, data in network_graph.edges(data=True):
+        x0, y0 = pos[u]
+        x1, y1 = pos[v]
+        fig.add_trace(go.Scatter(x=[x0, x1], y=[y0, y1], mode='lines',line=dict(width=.1+(data["weight"]/max_weight)*2, color='rgba(127,127,127,255)')))
+    for node in network_graph.nodes():
+        x, y = pos[node]
+        fig.add_trace(go.Scatter(
+            x=[x], y=[y], 
+            name=node, 
+            text=node+"\n"+str(debug_data[node]), 
+            mode='markers',
+            marker=dict(
+                size=6,
+                color=node_color_function(node),
+                line=dict(
+                    color='black',
+                    width=1))))
+    fig.update_layout(go.Layout(
+        title="Network graph "+title,
+        showlegend=False,
+        hovermode='closest',
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)'
+    ))
+    fig.show()
+
+
+
+
+print_graph("",color_centrality,centrality_metric)
